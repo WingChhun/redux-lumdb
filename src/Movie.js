a719c68ea72a341925f1acc35f89e7bcac7b03dc
WingChhun <capt.wing.chhun@gmail.com>
2018-08-22 06:41:04 -0700

    Added starter comments to help me remember the architecture

--- src/Movie.js
+++ src/Movie.js
@@ -1,27 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import styled from 'styled-components';
 import Overdrive from 'react-overdrive';
 
 const POSTER_PATH = 'http://image.tmdb.org/t/p/w154';
 
-const Movie = ({ movie }) => (
-  <Link to={`/${movie.id}`}>
-    <Overdrive id={`${movie.id}`}>
-      <Poster src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
-    </Overdrive>
-  </Link>
+// TODO: Import Link, will Sync with React-Router-Dom and render the MovieDetail
+// Component
+const Movie = ({movie}) => (
+    <Link to={`/${movie.id}`}>
+        <Overdrive id={`${movie.id}`}>
+            <Poster src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title}/>
+        </Overdrive>
+    </Link>
 );
 
 export default Movie;
 
 Movie.propTypes = {
-  movie: PropTypes.shape({
-    title: PropTypes.string.isRequired,
-  }).isRequired,
+    movie: PropTypes
+        .shape({title: PropTypes.string.isRequired})
+        .isRequired
 };
 
-export const Poster = styled.img`
+export const Poster = styled.img `
   box-shadow: 0 0 35px black;
 `;
