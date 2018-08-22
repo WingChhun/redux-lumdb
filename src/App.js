cfbb7a69415913eb20fb932d1ff0cb18501192d1
WingChhun <capt.wing.chhun@gmail.com>
2018-08-22 15:18:22 -0700

    Install redux-localstorage-simple and apply to App store

--- src/App.js
+++ src/App.js
@@ -4,20 +4,20 @@ import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
 import {composeWithDevTools} from 'redux-devtools-extension';
 import logger from 'redux-logger';
-
-import logo from './logo.svg';
-import './App.css';
-
 import thunk from 'redux-thunk';
+import {save, load} from 'redux-localstorage-simple';
+
 import MoviesListContainer from './components/Movies/MoviesList.container.jsx';
 import MovieDetailContainer from './components/Movies/MovieDetail.container.jsx';
 import ToggleContainer from "./components/Toggle/Toggle.container";
 import rootReducer from "./rootReducer";
 
+import logo from './logo.svg';
+import './App.css';
 //TODO: rootReducer , initialState, middleWare, composeWithDevtools
 
 const middleware = [logger, thunk];
-const store = createStore(rootReducer, {}, composeWithDevTools(applyMiddleware(...middleware)));
+const store = createStore(rootReducer, load(), composeWithDevTools(applyMiddleware(...middleware, save())));
 
 const App = () => (
     <Provider store ={store}>
