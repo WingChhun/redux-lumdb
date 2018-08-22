edb5ef8f6ac6047c8748e608ab33ac11ac7ff098
WingChhun <capt.wing.chhun@gmail.com>
2018-08-22 07:58:52 -0700

    import Provider wrap Router

--- src/App.js
+++ src/App.js
@@ -1,30 +1,35 @@
 /* eslint react/no-did-mount-set-state: 0 */
-import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Link,
-} from 'react-router-dom';
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+
 import logo from './logo.svg';
 import './App.css';
 
 import MoviesList from './MoviesList';
 import MovieDetail from './MovieDetail';
+
+const defaultState = () => ({welcome: "starter"});
+
+const store = createStore(defaultState);
+
 const App = () => (
-  <Router>
-    <div className="App">
-      <header className="App-header">
-        <Link to="/">
-          <img src={logo} className="App-logo" alt="logo" />
-        </Link>
-      </header>
-      <Switch>
-        <Route exact path="/" component={MoviesList} />
-        <Route path="/:id" component={MovieDetail} />
-      </Switch>
-    </div>
-  </Router>
+    <Provider store ={store}>
+        <Router>
+            <div className="App">
+                <header className="App-header">
+                    <Link to="/">
+                        <img src={logo} className="App-logo" alt="logo"/>
+                    </Link>
+                </header>
+                <Switch>
+                    <Route exact path="/" component={MoviesList}/>
+                    <Route path="/:id" component={MovieDetail}/>
+                </Switch>
+            </div>
+        </Router>
+    </Provider>
 );
 
 export default App;
